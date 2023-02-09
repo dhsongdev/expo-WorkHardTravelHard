fd4bc572fb347a4ba6d5c816af821a86b0a8e2ce
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2023-02-10 02:01:52 +0900

    create header

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello ;)</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnTxt}>Work</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.btnTxt}>Travel</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.body}></View>
     </View>
   );
 }
@@ -16,5 +24,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  header: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "grey",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  btnTxt: {
+    color: "white",
+    fontSize: "30%",
+    fontWeight: "700",
+    marginTop: 40,
+    padding: 30,
+  },
+  body: {
+    flex: 6,
   },
 });
