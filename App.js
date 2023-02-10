0435bddd7afb26c78f72b7370b56af44ea8e223a
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2023-02-10 16:24:17 +0900

    add textInput

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  View,
+} from "react-native";
+
+import React, { useState } from "react";
 
 export default function App() {
+  //useStates
+  const [selected, setSelected] = useState("work");
+  const [text, setText] = useState("");
+
+  //header Work or Travel Button
+  const work = () => setSelected("work");
+  const travel = () => setSelected("travel");
+
+  //value 값 활용하기 위해 필수
+  const onChangeText = (e) => {
+    console.log(e.nativeEvent.text);
+    setText(e.nativeEvent.text);
+  };
+
+  const addTodo = (e) => {
+    alert(`다음을 추가합니다.
+    ${text}`);
+    setText("");
+  };
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={work}>
           <Text style={styles.btnTxt}>Work</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.btnTxt}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text editable style={styles.btnTxt}>
+            Travel
+          </Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.body}></View>
+      <View style={styles.body}>
+        <TextInput
+          style={[styles.textInput, styles.todoInput]}
+          placeholder={
+            selected === "work" ? "Add Work Todo" : "Add Travel Todo"
+          }
+          onChange={onChangeText}
+          onSubmitEditing={addTodo}
+          value={text}
+        />
+      </View>
     </View>
   );
 }
@@ -22,8 +62,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   header: {
     flex: 1,
@@ -42,5 +80,17 @@ const styles = StyleSheet.create({
   },
   body: {
     flex: 6,
+    alignItems: "center",
+  },
+  textInput: {
+    borderStyle: "solid",
+    borderWidth: 0.5,
+    borderColor: "grey",
+    borderRadius: 30,
+    padding: 15,
+  },
+  todoInput: {
+    width: "90%",
+    marginTop: 20,
   },
 });
